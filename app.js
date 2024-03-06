cfaddd3cd4a966a9270db6def006e4804dc4abd6
samarth-thaker <samarth.thaker07@gmail.com>
2024-03-06 20:15:32 +0530

    Final Frontend

--- app.js
+++ app.js
@@ -1,57 +1,118 @@
-let card = document.querySelectorAll(".work");
-let card2 = document.getElementById("workSec");
-let blog = document.querySelector(".work");
+
+let card = document.querySelector(".trend");
+let card2 = document.getElementById("trendSec");
 let about = document.querySelector(".about");
+let contact = document.querySelector(".contact");
+
+
+console.log(card);
+let blog = document.querySelector(".trends");
 let mainPage = document.querySelector(".main");
-let workContainer = document.querySelector(".work");
-function Home(){
+
+function homes(){
     mainPage.style.display="flex";
     card.style.display="block";
-    card2.style.display="block";   
-    blog,style.display="block";
-    document.getElementById("Blogs").style.color="black";
-    document.getElementById("Home").style.color="rgb(222, 184, 135)";
-    document.getElementById("Explore").style.color="black";
-    document.getElementById("AboutUs").style.color="black";
-    document.getElementById("Contact").style.color="black";
-}
-function Explore(){
-     mainPage.style.display = "none";
-     blog.style.display = "block";
-    about.style.display="none"
-    card.style.display="block";
     card2.style.display="block";
-    document.getElementById("Blogs").style.color="black";
-    document.getElementById("Home").style.color="black";
-    document.getElementById("Explore").style.color="rgb(222, 184, 135)";
-    document.getElementById("AboutUs").style.color="black";
-    document.getElementById("Contact").style.color="black";
-}
-function Blogs(){
-    mainPage.style.display = "none";
-    card.style.display = "none";
-    card2.style.display="none";
     blog.style.display="block";
-    about.style.display="none"
-    document.getElementById("Blogs").style.color="rgb(222, 184, 135)";
-    document.getElementById("Home").style.color="black";
-    document.getElementById("Explore").style.color="black";
-    document.getElementById("AboutUs").style.color="black";
-    document.getElementById("Contact").style.color="black";
+   about.style.display="none"
 
-}
-function AboutUs(){
-    mainPage.style.display = "none";
-    card2.style.display="none";
-    blog.style.display="none";
-    about.style.display="block";
-    workContainer.style.display="none";
-
-    document.getElementById("Blogs").style.color="black";
-    document.getElementById("Home").style.color="black";
-    document.getElementById("Explore").style.color="black";
-    document.getElementById("AboutUs").style.color="rgb(222, 184, 135)";
-    document.getElementById("Contact").style.color="black";
- } 
 
     
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="rgb(222, 184, 135)";
+document.getElementById("explore").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+
+}
+function explore(){
+mainPage.style.display="none";
+blog.style.display="none";
+about.style.display="none"
+card.style.display="block";
+card2.style.display="block";
+
+ 
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("explore").style.color="rgb(222, 184, 135)";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+
+}
+
+function blogs(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="block";
+   about.style.display="none"
+    
+
+document.getElementById("blog").style.color="rgb(222, 184, 135)";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+
+ 
+
+
+
+}
+
+function abouts(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="block"
+
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("shop").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="rgb(222, 184, 135)"
+
+}
+
+function contacts(){
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="none";
+   contact.style.display="block"
+
+document.getElementById("blog").style.color="black";
+document.getElementById("home").style.color="black";
+document.getElementById("explore").style.color="black";
+document.getElementById("contact").style.color="black";
+document.getElementById("about").style.color="black"
+document.getElementById("contact").style.color="rgb(222, 184, 135)"
+
+}
+
+// cart
+
+function show(img){
+    let newImg = document.getElementById("newImg");
+    console.log(img);
+    newImg.src=img.src;
+
+    mainPage.style.display="none";
+    card.style.display="none";
+    card2.style.display="none";
+   blog.style.display="none";
+   about.style.display="none";
+   contact.style.display="none"
+    document.querySelector(".cart").style.display="flex"
+}
+
+
+function addCart(){
+    alert("Added To Cart");
+    location.reload()
+}
